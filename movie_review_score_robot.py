cb009d8f2f75a8fa590c44f1a83847e41a590975
Kissaritari <kissaritari+github@protonmail.com>
2024-12-16 01:06:38 +0200

    button

--- movie_review_score_robot.py
+++ movie_review_score_robot.py
@@ -25,6 +25,10 @@ def parse_input_email() -> str:
 def get_movie_summary(title) -> str:
     """Get the movie summary from rotten tomatoes. The site search does not work well with years, so it is left out here."""
     browser.goto("https://www.rottentomatoes.com/")
+    try: # try to click the conset the cookies button
+        browser.page().click('//button[text()="I accept"]')
+    except:
+        pass # pass if the banner is not found
     search_selector = "#header-main > search-results-nav:nth-child(3) > search-results-controls:nth-child(1) > input:nth-child(1)"  # selectors generated with firefox inspect
     browser.page().fill(search_selector, title)
     first_result_selector = "#header-main > search-results-nav:nth-child(3) > search-results:nth-child(2) > search-results-item:nth-child(2) > rt-link:nth-child(2)"
