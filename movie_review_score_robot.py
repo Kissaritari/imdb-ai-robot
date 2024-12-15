e7aaba4d016730a4e369723fc23695ea9535a707
Kissaritari <kissaritari+github@protonmail.com>
2024-12-15 23:39:33 +0200

    removed .env

--- movie_review_score_robot.py
+++ movie_review_score_robot.py
@@ -7,17 +7,12 @@ from gemini_handler import GeminiHandler
 
 @task
 def get_movie_review_score_from_email():
-    environment = os.getenv("ENV", "local")
-    print(environment)
-    if environment == "local":
-        os.environ["RPA_SECRET_MANAGER"] = "RPA.Robocorp.Vault.FileSecrets"
-        os.environ["RPA_SECRET_FILE"] = "secrets.json"
 
     gemini = GeminiHandler()
     mail = EmailHandler()
 
     for item in workitems.inputs:
-        workitems.outputs.create(payload={"key": "value"})
+        workitems.outputs.create(payload={"key": "value"}) 
         print(item.payload)
         get_movie_summary(item.payload)
         gemini.get_review_score(item.payload)
